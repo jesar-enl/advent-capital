b757d2643ad7ed1c0891f7aefb9cc8bcf48db1bf
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-12-12 13:47:29 +0300

    loan page details

--- app/loans/[id]/page.jsx
+++ app/loans/[id]/page.jsx
@@ -1,4 +1,5 @@
 import { Loan } from '../../libs/loan';
+import Link from 'next/link'
 
 export default async function LoanDetails({ params: { id } }) {
   const application = await Loan(id);
@@ -6,6 +7,9 @@ export default async function LoanDetails({ params: { id } }) {
   return (
     <div className="flex flex-col mt-28">
       <div className="flex gap-3 items-center">
+        <Link href="/dashboard">
+          
+        </Link>
         <h2 className="text-3xl text-teal-800">
           Loan Application: #{application.id}
         </h2>
