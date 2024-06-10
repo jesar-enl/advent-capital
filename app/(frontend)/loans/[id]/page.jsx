9ff6bc1b19b38cb849c2513d86c78550fce6b135
Jesse Kyambadde <jessekyambadde0@gmail.com>
2024-06-10 14:16:50 +0300

    made button CSR

--- app/(frontend)/loans/[id]/page.jsx
+++ app/(frontend)/loans/[id]/page.jsx
@@ -3,50 +3,15 @@ import { authOptions } from '@/app/libs/authOptions';
 import { Loan } from '@/app/libs/loan';
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
-import toast from 'react-hot-toast';
 import { FaArrowCircleLeft } from 'react-icons/fa';
 import { RxCross2 } from 'react-icons/rx';
-import { TiTick } from 'react-icons/ti';
 import Printable from '../../../components/Printable';
+import ApproveButton from '../../../components/button';
+
 
 export const metadata = {
   title: 'Loan Application - Advent Capital',
   description: 'Apply now to get a loan that suits your needs and goals.',
-};
-
-const ApproveButton = ({ id }) => {
-  const handleUpdateStatus = async () => {
-    try {
-      const response = await fetch(`/api/application/${id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ status: 'APPROVED' }),
-      });
-
-      if (response.ok) {
-        toast.success('Status updated successfully.');
-        redirect('/accept-letter');
-      } else {
-        toast.error('Error updating status');
-        console.error('Error updating status:', response.status);
-      }
-    } catch (error) {
-      toast.error('Error updating status');
-      console.error('Error updating status:', error);
-    }
-  };
-
-  return (
-    <button
-      type="button"
-      onClick={handleUpdateStatus}
-      className="inline-flex items-center px-3 py-1 text-xl font-medium text-gray-900 bg-white border-t border-b border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-500 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:text-blue-500"
-    >
-      <TiTick className="me-2 text-green-600 text-xl" />
-      Approve
-    </button>
-  );
 };
 
 export default async function LoanDetails({ params: { id } }) {
---
+++ app/components/button.jsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { TiTick } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+import { redirect } from 'next/navigation';
+import { useState } from 'react';
+
+
+const ApproveButton = ({ id }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleUpdateStatus = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(`/api/application/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ status: 'APPROVED' }),
+      });
+
+      if (response.ok) {
+        toast.success('Status updated successfully.');
+        redirect('/accept-letter');
+      } else {
+        toast.error('Error updating status');
+        console.error('Error updating status:', response.status);
+      }
+    } catch (error) {
+      toast.error('Error updating status');
+      console.error('Error updating status:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleUpdateStatus}
+      className="inline-flex items-center px-3 py-1 text-xl font-medium text-gray-900 bg-white border-t border-b border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-500 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:text-blue-500"
+      disabled={loading}
+    >
+      <TiTick className="me-2 text-green-600 text-xl" />
+      Approve
+    </button>
+  );
+};
+
+export default ApproveButton;
