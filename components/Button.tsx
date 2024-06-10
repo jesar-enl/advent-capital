0cbd9468f156c60d955ad39a5c6b7a4ffdcb56e7
Jesse Kyambadde <jessekyambadde0@gmail.com>
2024-06-10 11:10:31 +0300

    redirect function on button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,13 @@
-'use client';
 import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 import { TiTick } from 'react-icons/ti';
 
 export function AcceptButton() {
-  const router = useRouter();
+  
   return (
     <Button
       type="button"
-      onClick={() => router.push('/accept-letter')}
+      onClick={redirect('/accept-letter')}
       className="inline-flex items-center px-4 py-2 text-xl font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
     >
       <TiTick className="me-2 text-green-600 text-xl" />
