7b8dc60628e33b7872bfbb7eb28a6d5993fbfe56
Jesse Kyambadde <jessekyambadde0@gmail.com>
2024-06-12 17:34:16 +0300

    not-found page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,18 +1,36 @@
 import Link from 'next/link'
-import { headers } from 'next/headers'
  
-export default async function NotFound() {
-  const headersList = headers()
-  const domain = headersList.get('host')
-  const data = await getSiteData(domain)
-  
+export default function NotFound() {  
+
   return (
-    <div className="container tracking-widest">
-      <h2 className="text-xl md:text-2xl font-semibold">Not Found: {data.name}</h2>
-      <p className="text-base md:text-lg">Could not find requested resource</p>
-      <p>
-        <Link href="/dashboard">Go to dashboard</Link>
-      </p>
-    </div>
+    <div className='w-4/5 mx-auto mt-20 flex flex-col justify-center items-center space-y-4'>
+        <h1 className='text-4xl font-semibold'>404 - Page Not Found</h1>
+        <h4 className='py-10 text-lg text-red-500 font-semibold capitalize'>
+          The Nothingness and Emptiness is here...
+        </h4>
+        <p>
+          Use the links below to explore our amazing application
+        </p>
+        <div className='space-x-4'>
+          <Link
+            className='underline text-blue-600 hover:text-red-500 duration-300'
+            href='/'
+          >
+            Homepage
+          </Link>
+          <Link
+            className='underline text-blue-600 hover:text-red-500 duration-300'
+            href='/dashboard'
+          >
+            Dashboard
+          </Link>
+          <Link
+            className='underline text-blue-600 hover:text-red-500 duration-300'
+            href='/contact-us'
+          >
+            Contact Us
+          </Link>
+        </div>
+      </div>
   )
 }
